88cb5a61241f1cd244fc79d4322a17fc05399bb1
jayden owens <jaydenowens23@gmail.com>
2021-06-23 22:25:28 -0600

    fix please

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -21,7 +21,6 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
                     </div>
                     <img
                       src='img/portfolio/01-small.jpg'
@@ -42,7 +41,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
+                      
                     </div>
                     <img
                       src='img/portfolio/02-small.jpg'
@@ -63,7 +62,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
+                     
                     </div>
                     <img
                       src='img/portfolio/03-small.jpg'
@@ -84,7 +83,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
+                      
                     </div>
                     <img
                       src='img/portfolio/04-small.jpg'
@@ -105,7 +104,6 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
                     </div>
                     <img
                       src='img/portfolio/05-small.jpg'
@@ -126,7 +124,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
+
                     </div>
                     <img
                       src='img/portfolio/06-small.jpg'
@@ -147,7 +145,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
+                      
                     </div>
                     <img
                       src='img/portfolio/07-small.jpg'
@@ -168,7 +166,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
+                      
                     </div>
                     <img
                       src='img/portfolio/08-small.jpg'
@@ -189,7 +187,7 @@ export const Gallery = (props) => {
                     data-lightbox-gallery='gallery1'
                   >
                     <div className='hover-text'>
-                      <h4></h4>
+                     
                     </div>
                     <img
                       src='img/portfolio/09-small.jpg'
